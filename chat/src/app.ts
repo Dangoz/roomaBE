dcd2359ee3fe0aaf0bd5d4dedd20a42b3516671f
Dangoz <71613713+Dangoz@users.noreply.github.com>
2021-10-17 23:27:32 -0700

    init services with compose

---
+++ chat/src/app.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import IController from "@/interfaces/controller.interface";
+
+class App {
+  private _app: express.Application;
+  private readonly _port: number | string = process.env.PORT || 3000;
+
+  constructor(public controllers: IController[]) {
+    this._app = express();
+
+    this.initializeMiddlewares();
+    this.initializeControllers();
+  }
+
+  public start() {
+    this._app.listen(this._port, () => {
+      console.log(`template listening on the port ${this._port}`);
+    });
+  }
+
+  private initializeMiddlewares() {
+    require("@/middlewares/express.middleware")(this._app);
+  }
+
+  private initializeControllers() {
+
+    // pass in each router from controllers
+    this.controllers.forEach(controller => {
+      this._app.use('/', controller.router);
+    })
+  }
+}
+
+export default App;
