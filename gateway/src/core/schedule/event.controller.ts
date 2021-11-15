8e17d483cf5efbc77206e83d950d75218a5d6848
Dangoz <71613713+Dangoz@users.noreply.github.com>
2021-11-15 12:42:49 -0800

    gateway: delete event route

--- gateway/src/core/schedule/event.controller.ts
+++ gateway/src/core/schedule/event.controller.ts
@@ -3,6 +3,7 @@ import { ensureAuthenticated } from "@/middlewares/authen.middleware";
 import express, { Request, Response, NextFunction } from "express";
 import { schedule } from "@/configs/rest";
 import { checkRoomId } from "@/middlewares/check.middleware";
+import { AxiosError } from "axios";
 
 class EventController implements IController {
   public path = "/v1/event";
@@ -16,6 +17,7 @@ class EventController implements IController {
     this.router.get(`${this.path}/list`, ensureAuthenticated, checkRoomId, this.getEvents);
     this.router.post(`${this.path}/create`, ensureAuthenticated, checkRoomId, this.createEvent);
     this.router.patch(`${this.path}/update`, ensureAuthenticated, checkRoomId, this.updateEvent);
+    this.router.delete(`${this.path}/delete/:eid`, ensureAuthenticated, checkRoomId, this.deleteEvent);
   }
 
   private getEvents = async (req: Request, res: Response) => {
@@ -50,6 +52,17 @@ class EventController implements IController {
       const eventRes = await schedule.patch(`/event/update`, req.body);
       const event = eventRes.data.event;
       res.status(200).json({ message: "event updated", event });
+    } catch (error) {
+      console.error((error as Error).message);
+      res.status(500).json({ message: "server error" });
+    }
+  }
+
+  private deleteEvent = async (req: Request, res: Response) => {
+    try {
+      const eventRes = await schedule.delete(`/event/delete/${req.params.eid}`);
+      const event = eventRes.data.event;
+      res.status(200).json({ message: "event deleted", event });
     } catch (error) {
       console.error((error as Error).message);
       res.status(500).json({ message: "server error" });
